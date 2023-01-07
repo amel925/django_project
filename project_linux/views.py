87ffc9326a47058da74bd7e0f7a73b871251f21a
amel925 <amelnaitamer7@gmail.com>
2023-01-07 22:34:36 +0100

    reglage de l'affichage

--- project_linux/views.py
+++ project_linux/views.py
@@ -17,6 +17,7 @@ def index(request):
     url="https://odre.opendatasoft.com/api/records/1.0/search/?dataset=prod-nat-gaz-horaire-prov&q=&rows=10000&sort=journee_gaziere&timezone=Europe%2FParis"
     url2="https://odre.opendatasoft.com/api/records/1.0/search/?dataset=prod-nat-gaz-horaire-prov"
     r=requests.get(url2)
+
     #r=requests.get('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=1000&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes&facet=coverflow&facet=creditcard&facet=overflowactivation&facet=kioskstate&facet=station_state')
     velib=r.json()
 
@@ -26,12 +27,19 @@ def index(request):
     dbname.test_collection.delete_many({})
     dbname.test_collection.insert_many(velib['records'])
     data=dbname.test_collection.find()
-    data1=[[d["fields"]["prod_journaliere_mwh_pcs"] for d in data]]
+    data1=[]
+    data2=[]
+
+    for d in data:
+        data1.append(d["fields"]["prod_journaliere_mwh_pcs"])
+        data2.append(d["fields"]["journee_gaziere"])
+    #data1=[[d["fields"]["journee_gaziere"] for d in data]]
    
     print(data1)
     #return HttpResponse("Hello, world. You're at the index.")
     context={
         'data':data1,
+        'dataa':data2,
     }
     print("ici ",context)
     return render(request, 'dashboard/index.html',context)
