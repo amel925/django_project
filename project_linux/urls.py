bd1ee32a0be4a3e4c8d083d1c39fee79a44ec40a
amel925 <amelnaitamer7@gmail.com>
2023-01-09 00:55:27 +0100

    add 2 pages

--- project_linux/urls.py
+++ project_linux/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index2, name='index'),
-    #path('hell', views.index2, name='index2'),
+    path('gaz', views.index, name='index'),
+    path('electricite', views.index2, name='index2'),
 ]
